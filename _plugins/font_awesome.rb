1a04257ddeee559962731803bdbd0bd3d1c10161
SimoneCnt <simonecnt@gmail.com>
2015-05-18 02:10:45 +0200

    Update style

---
+++ _plugins/font_awesome.rb
@@ -0,0 +1,67 @@
+##
+# The MIT License (MIT)
+# 
+# Copyright (c) 2014 Ryan Morrissey
+# Copyright (c) 2015 Simone Conti
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+# 
+# 
+# Font Awesome Icons Liquid Tag
+# Documentation can be found at http://fontawesome.io/
+#
+# Example:
+#    {% icon fa-camera-retro %}
+#    {% icon fa-camera-retro fa-lg %}
+#    {% icon fa-spinner fa-spin %}
+#    {% icon fa-shield fa-rotate-90 %}
+#
+# Added support for Academicons, see http://jpswalsh.github.io/academicons/
+# Example:
+#    {% icon ai-researchgate-square fa-3x %}
+#
+# Added support for multiple-argument like:
+#    {% icon fa-spinner fa-pulse fa-4x fa-fw margin-bottom %}
+#
+
+module Jekyll
+  class FontAwesomeTag < Liquid::Tag
+ 
+    def render(context)
+        icon_flags = @markup
+        icon_label = @markup.strip.split("-")[0]
+        if icon_label=="fa" or icon_label=="ai" 
+            "<i class=\"#{icon_label} #{icon_flags}\"></i>"
+        else
+            raise ArgumentError.new <<-eos
+Syntax error in tag 'icon' while parsing the following markup:
+
+  #{@markup}
+
+Valid syntax:
+  for icons: {% icon fa-camera-retro %}
+  for icons with size/spin/rotate: {% icon fa-camera-retro fa-lg %}
+eos
+        end
+    end
+ 
+  end
+end
+ 
+Liquid::Template.register_tag('icon', Jekyll::FontAwesomeTag)
